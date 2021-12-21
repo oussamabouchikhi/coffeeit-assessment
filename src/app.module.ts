5a2c86c0907d67e26e43544213932e74d3314276
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2021-12-21 20:31:42 +0100

    🗃️ Configure and connect to MongoDB Atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.MONGODB_URI,
+      ssl: true,
+      useNewUrlParser: true,
+      entities: [__dirname + '/../**/*.entity{.ts|.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
